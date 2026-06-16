f4a807ad515319afdbddeaccf595e137806a4df2
Ritikgupta-tech <ritik95061@gmail.com>
2026-06-16 14:41:07 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const app = express();
+const mongoose= require("mongoose");
+const Listing= require("./models/listing.js");
+const path = require("path");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+    .then(()=>{
+        console.log("connect to db");
+    })
+    .catch((err) =>{
+        console.log(err);
+    });
+    async function main() {
+        await mongoose.connect(MONGO_URL);
+    }
+
+    app.set("view engine","ejs");
+    app.set("views",path.join(__dirname,"views"));
+    app.use(express.urlencoded({extended:true}));
+
+
+app.get("/",(req,res)=>{
+    res.send("hi,i am root");
+});
+
+//index route
+app.get("/listings",async(req,res)=>{
+   const allListings=await Listing.find({});
+   res.render("listings/index.ejs",{allListings});
+    });
+// show route
+app.get("/listing/:id",async(req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+})
+/*
+app.get ("/testListing", async(req,res)=>{
+    let sampleListing = new Listing({
+        title:"my new vila",
+        description:"by the bench",
+        price:1200,
+        location:"calgute,goa",
+        country:"india"
+    });
+     await sampleListing.save();
+     console.log("sample was saved");
+     res.send("successful testing");
+});
+*/
+app.listen(3000,() =>{
+    console.log("server is listening to port 30000");
+});
