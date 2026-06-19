ea1f01550fea477da5566b51e5dbc65d949a4cc7
Ritikgupta-tech <ritik95061@gmail.com>
2026-06-20 00:03:18 +0530

    day-101

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const methodOverride= require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-
+const {listingSchema} = require("./schema.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -33,6 +33,17 @@ app.get("/",(req,res)=>{
     res.send("hi,i am root");
 });
 
+const validateListing= (req,res,next) =>{
+     let {error}= listingSchema.validate(req.body);
+
+   if(error){
+    let errMsg= error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400,errMsg);
+   } else {
+    next();
+   }
+};
+
 //index route
 app.get("/listings",wrapAsync(async(req,res)=>{
    const allListings=await Listing.find({});
@@ -54,10 +65,8 @@ app.get("/listings/:id",wrapAsync(async(req,res)=>{
 }));
 
 // create route
-app.post("/listings",wrapAsync(async (req,res,next) =>{
-     if(!req.body.listing){
-        throw new ExpressError(400,"send valid data for listing");
-    }
+app.post("/listings",validateListing,wrapAsync(async (req,res,next) =>{
+  
    const newListing= new Listing(req.body.listing);
    await newListing.save();
    res.redirect("/listings");
@@ -75,10 +84,8 @@ app.get("/listings/:id/edit",wrapAsync (async(req,res) =>{
 }));
 
 // update route
-app.put("/listings/:id" ,wrapAsync (async(req,res)=>{
-    if(!req.body.listing){
-        throw new ExpressError(400,"send valid data for listing");
-    }
+app.put("/listings/:id" ,validateListing,wrapAsync (async(req,res)=>{
+   
     let {id} = req.params;
    await Listing.findByIdAndUpdate(id, {...req.body.listing});
    res.redirect(`/listings/${id}`);
